cd5edfaaf1a8a07651b9a3a80f667d31d4744839
Casamento Isadora & Pedro <casamento@pepeisa.com.br>
2026-06-01 00:06:13 +0000

    Fix: imagens paletas com fundo branco e tamanho ajustado

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -304,9 +304,9 @@ export default function Home() {
                     </p>
                     <div className="flex justify-center">
                       <img
-                        src="https://files.manuscdn.com/user_upload_by_module/session_file/310519663341133733/roJMHOViESFQiWAz.png"
+                        src="https://files.manuscdn.com/user_upload_by_module/session_file/310519663341133733/qcakqcAznNwrfddK.png"
                         alt="Paleta de cores dos padrinhos"
-                        className="max-w-full w-auto max-h-40 object-contain"
+                        className="max-w-full w-auto max-h-28 object-contain"
                       />
                     </div>
                   </div>
@@ -325,7 +325,7 @@ export default function Home() {
                     </p>
                     <div className="flex justify-center">
                       <img
-                        src="https://files.manuscdn.com/user_upload_by_module/session_file/310519663341133733/pCmzuYXtRqaJxcEk.png"
+                        src="https://files.manuscdn.com/user_upload_by_module/session_file/310519663341133733/fVBEPNaODkJPPMwb.png"
                         alt="Paleta de cores das madrinhas"
                         className="max-w-full w-auto max-h-56 object-contain"
                       />
